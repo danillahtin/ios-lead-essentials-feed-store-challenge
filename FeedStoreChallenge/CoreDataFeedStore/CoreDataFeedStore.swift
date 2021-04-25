c673f790b56f6a2cda8a65c886890ce156fb06a2
Danil Lakhtin <danil_lahtin@mail.ru>
2021-04-25 19:34:01 +0300

    Replace in-memory implementation with CoreData

--- FeedStoreChallenge/CoreDataFeedStore/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore/CoreDataFeedStore.swift
@@ -10,40 +10,105 @@ import Foundation
 import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
-    private final class FeedCache {
-        let feed: [LocalFeedImage]
-        let timestamp: Date
-
-        init(feed: [LocalFeedImage], timestamp: Date) {
-            self.feed = feed
-            self.timestamp = timestamp
-        }
-    }
-
-    private var cache: FeedCache?
-    private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
 
     public init(storeUrl: URL, bundle: Bundle = .main) throws {
-        container = try .load(modelName: "ManagedCacheModel", at: storeUrl, in: bundle)
+        let container = try NSPersistentContainer.load(modelName: "ManagedCacheModel", at: storeUrl, in: bundle)
         context = container.newBackgroundContext()
     }
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        cache = nil
-        completion(.none)
+        context.perform { [context] in
+            do {
+                try context.findCache()
+                    .map({ context.delete($0) })
+                    .map({ try context.save() })
+
+                completion(.none)
+            } catch {
+                completion(error)
+            }
+        }
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        cache = FeedCache(feed: feed, timestamp: timestamp)
-        completion(.none)
+        context.perform { [context] in
+            do {
+                try context.findCache()
+                    .map({ context.delete($0) })
+                    .map({ try context.save() })
+
+                let cache = ManagedCache(with: feed, timestamp: timestamp, in: context)
+                context.insert(cache)
+                try context.save()
+
+                completion(.none)
+            } catch {
+                completion(error)
+            }
+        }
     }
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        guard let cache = cache else {
-            return completion(.empty)
+        context.perform { [context] in
+            do {
+                guard let cache = try context.findCache() else {
+                    return completion(.empty)
+                }
+
+                completion(.found(feed: cache.makeLocalFeedImages(), timestamp: cache.timestamp!))
+            } catch {
+                completion(.failure(error))
+            }
         }
-        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+    }
+}
+
+// MARK: - Helpers
+
+private extension ManagedFeedImage {
+    static func makeFromLocalFeedImage(in context: NSManagedObjectContext) -> (LocalFeedImage) -> ManagedFeedImage {
+        return {
+            let managed = ManagedFeedImage(context: context)
+
+            managed.id = $0.id
+            managed.details = $0.description
+            managed.location = $0.location
+            managed.url = $0.url
+
+            return managed
+        }
+    }
+
+    func makeLocalFeedImage() -> LocalFeedImage {
+        LocalFeedImage(
+            id: id!,
+            description: details,
+            location: location,
+            url: url!
+        )
+    }
+}
+
+private extension ManagedCache {
+    convenience init(with feed: [LocalFeedImage], timestamp: Date, in context: NSManagedObjectContext) {
+        self.init(context: context)
+
+        self.images = NSOrderedSet(array: feed.map(ManagedFeedImage.makeFromLocalFeedImage(in: context)))
+        self.timestamp = timestamp
+    }
+
+    func makeLocalFeedImages() -> [LocalFeedImage] {
+        images!
+            .map({ $0 as! ManagedFeedImage })
+            .map({ $0.makeLocalFeedImage() })
+    }
+}
+
+private extension NSManagedObjectContext {
+    func findCache() throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        return try fetch(request).first
     }
 }
 
